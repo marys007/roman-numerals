c8404f3d4a3ed70ab757abc19a37418ccf856bda
marys007 <148959467+marys007@users.noreply.github.com>
2023-11-09 11:17:12 +0100

    docstrings

--- python/test_roman_numerals.py
+++ python/test_roman_numerals.py
@@ -2,6 +2,10 @@ import unittest
 
 
 class RomanNumeralsTest(unittest.TestCase):
+    """
+    Class convert arabic number to roman format of number
+    
+    """
     def test_arabic_1_converts_to_roman_I(self):
         actual = convert(1)
         self.assertEqual(actual, "I")
